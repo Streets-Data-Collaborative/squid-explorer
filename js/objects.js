8089d1675316c3de335394bf25189c7ab568a94d
thenamesdave <dmmarulli@gmail.com>
2017-11-18 20:54:29 -0800

    made stable

---
+++ js/objects.js
@@ -0,0 +1,139 @@
+////// visualization state object and global data object
+state = {
+	"featureID" : "<UNIQUE_IDENTIFIER>",
+	"minQuality" : "<MINIMUM_RIDE_QUALITY>",
+	"maxQuality" : "<MAX_RIDE_QUALITY>"
+}
+
+globals = {
+	"sublayers" : [],
+	"max" : "",
+	"min" : ""
+}
+
+function dataSetup(callback) {
+	query = `
+	SELECT *
+	FROM ${config.geometry_table}`;
+	encoded_query = encodeURIComponent(query);
+	url = `https://${config.account}.carto.com/api/v2/sql?q=${encoded_query}`
+	$.getJSON(url, function(idxData) {
+		var	min = 0,
+		max = idxData.total_rows,
+		randIdx =  Math.floor(Math.random() * (max - min)) + min,
+		randLocation = idxData.rows[randIdx]['cartodb_id'];
+		state.featureID = randLocation
+
+		query = `
+		SELECT
+			max(${config.column_names.relative_ride_quality}) mx,
+			min(${config.column_names.relative_ride_quality}) mn
+		FROM ${config.attribute_table}`,
+		encoded_query = encodeURIComponent(query),
+	url = `https://${config.account}.carto.com/api/v2/sql?q=${encoded_query}`;
+
+	$.getJSON(url, function(qualityData) {
+		globals.max = qualityData.rows[0]['mx'];
+		globals.min = qualityData.rows[0]['mn'];
+		callback();
+	});
+
+});
+
+}
+
+
+
+
+////// cartography object
+
+var cartography = {
+	"cartocss" : "<GEOM_DEPENDENT>",
+	"legend" : "<GEOM_DEPENDENT>",
+	'tooltip' :
+	`
+	<div class="cartodb-tooltip-content-wrapper light">
+	<div class="cartodb-tooltip-content">
+	<h3>Block-level</h3>
+	<h4>FIPS code</h4>
+	<p>{{hr_name}}</p>
+	<h4>Percent Over/Under Target</h4>
+	<p>{{percentdifference}}%</p>
+	<h4>Population</h4>
+	<p>{{population}}</p>
+	<h3>Group-level</h3>
+	<h4>Median Income</h4>
+	<p>\${{income}}</p>
+	<h4>Median Year Structure Built</h4>
+	<p>{{year_built}}</p>
+	<h4>Characteristic Education</h4>
+	<p>{{ed}}</p>
+	<h4>Primary Limited English Speaking Household Language</h4>
+	<p>{{limited_english}}</p>
+	</div>
+	</div>
+	`
+
+}
+
+// define legend
+// (should probably be contained within some cartographySetup() function for increased elegance)
+choropleth = new cdb.geo.ui.Legend({
+	type: "choropleth",
+	show_title: true,
+	title: "Ride Quality Rank",
+	data: [{
+		value: "0 -------- 20 -------- 40 ------- 60 ------- 80 ----- 100"
+	}, {
+		value: ""
+	}, {
+		name: "bin1",
+		value: "#D9534F"
+	}, {
+		name: "bin2",
+		value: "#D99F4F"
+	}, {
+		name: "bin3",
+		value: "#D9C24F"
+	}, {
+		name: "bin4",
+		value: "#B9D14C"
+	},		{
+		name: "bin5",
+		value: "#3EAB45"
+	}]
+})
+
+if (config.geom_type == "point") {
+	// var bubble = new cdb.geo.ui.Legend({
+	// 	type: "bubble",
+	// 	show_title: true,
+	// 	title: "District Population",
+	// 	data: [
+	// 		{ value: "Smallest" },
+	// 		{ value: "Largest" },
+	// 		{ name: "graph_color", value: "#ccc" }
+	// 		]
+	// 	})
+cartography.legend = new cdb.geo.ui.StackedLegend({
+	legends: [choropleth]
+})
+};
+
+// define cartocss
+if (config.geom_type == "point") {
+	cartography.cartocss =
+	`#table {
+		marker-fill-opacity: .75;
+		marker-line-width: 0;
+		marker-width: 10;
+		marker-allow-overlap: true;
+		polygon-comp-op: multiply;
+
+		#table [ ride_quality_score > 80] {marker-fill: #3EAB45;}
+		#table [ ride_quality_score <= 80] { marker-fill: #B9D14C; }
+		#table [ ride_quality_score <= 60] { marker-fill: #D9C24F; }
+		#table [ ride_quality_score <= 40] { marker-fill: #D99F4F; }
+		#table [ ride_quality_score <= 20] { marker-fill: #D9534F; }
+	}`
+}
---
+++ js/script.js
@@ -0,0 +1,207 @@
+// style components
+nav_height = $(".navbar").height();
+
+function styleSetup() {
+
+	$("#argoBrand")
+	.effect("shake", {
+		direction: "up",
+		distance: 5,
+		times: 3
+
+	}, 1000)
+	.animate({
+		color: "#CECECE"
+	},
+	1000
+	)
+	
+	// make sure correct section is highlighted
+	function selectSection(){
+		var window_top = 1.5*$(window).scrollTop();
+		var div_top = $('#tool').offset().top - nav_height;
+		if (window_top < div_top) {
+			makeSelected('#aboutLink')
+		} else {
+			makeSelected('#toolLink')
+		}
+	}
+
+	$(function() {
+		$(window).scroll(selectSection);
+		selectSection();
+	});
+
+	//	dynamic padding and div sizing on window resize
+	function dynamicPadding() {
+		if ($(window).width() < 993){
+			$(".halfleftpadding")
+			.removeClass("halfleftpadding")
+			.addClass("templeftPadding")
+
+			$(".halfrightpadding")
+			.removeClass("halfrightpadding")
+			.addClass("temprightPadding")
+
+		} else {
+			$(".templeftPadding")
+			.removeClass("templeftPadding")
+			.addClass("halfleftpadding")
+
+			$(".temprightPadding")
+			.removeClass("temprightPadding")
+			.addClass("halfrightpadding")
+		}
+	}
+
+	function dynamicSizing() {
+		// $("#extraUtility").css("min-height", `calc(100vh - ${nav_height}px)`);
+		$("#map, #detail, #imageContainer").css("height", `calc(100vh - ${nav_height}px)`);
+		$("#marker").css('line-height', `calc(100vh - ${nav_height}px - 30px)`);
+		$("body").css("padding-top", nav_height)
+
+	}
+
+	// dynamicPadding()
+	dynamicSizing()
+
+	$(window).resize(function(){
+		nav_height = $(".navbar").height();
+		dynamicSizing()
+		// dynamicPadding()
+	});
+};
+
+function makeSelected(element) {
+	$(".selected").removeClass("selected");
+	$(element).attr("class","selected");
+};
+
+function smoothScroll(divId){
+	$("html, body").animate({scrollTop:
+		$(divId).offset().top - nav_height}, 1000)
+};
+
+function transition(element, content){
+	$(element).fadeOut(function() {
+		$(element).html(content);
+		$(element).fadeIn();
+	});
+}
+
+
+// vizualization components
+function mapSetup() {
+	var map = new L.Map("map", {
+		center: config.coordinates,
+		zoom: config.zoom,
+		scrollWheelZoom:false
+	});
+
+	// Highlight feature setup below based on: http://bl.ocks.org/javisantana/d20063afd2c96a733002
+	var sql = new cartodb.SQL( {
+		user: config.account,
+		format: 'geojson' });
+	var polygon;
+
+	function showFeature(cartodb_id) {
+
+		sql.execute(`select ST_Centroid(the_geom) as the_geom from ${config.geometry_table} where cartodb_id = {{cartodb_id}}`, {cartodb_id: cartodb_id} )
+		.done(function(geojson) {
+			if (polygon) {
+
+				map.removeLayer(polygon);
+
+			}
+			polygon = L.geoJson(geojson, { 
+				style: {}
+			}).addTo(map);
+		});
+	}
+	// End highlight feature setup
+
+	var query = `
+		SELECT *,
+		TRUNC(100*((${config.column_names.relative_ride_quality} - ${globals.max}) / (${globals.min} - ${globals.max}))) ride_quality_score
+	 	FROM ${config.geometry_table}
+	 	ORDER BY ${config.column_names.relative_ride_quality}
+	 	`
+
+	var placeLayer = {
+		user_name: config.account,
+		type: 'cartodb',
+		sublayers: [{
+			sql: query,
+			cartocss: cartography.cartocss,
+			interactivity: [
+			config.column_names.date,
+			'ride_quality_score',
+			config.column_names.img_location,
+			'cartodb_id'
+			]
+		}]
+	};
+
+    // Pull tiles from OpenStreetMap
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    	attribution: 'Powered by <a href="http://www.argolabs.org/">ARGO</a> | &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors</a>'
+    }).addTo(map);
+
+    $("#map").append(cartography.legend.render().el);
+
+
+    cartodb.createLayer(map, placeLayer, options = {
+    	https: true
+    })
+    .addTo(map, 0)
+    .done(function(layer) {
+    	
+    	for (var i = 0; i < layer.getSubLayerCount(); i++) {
+    		globals.sublayers[i] = layer.getSubLayer(i);
+    	};
+    	globals.sublayers[0].setInteraction(true);
+    	globals.sublayers[0].on('featureClick', function(e, latlng, pos, data) {
+    		$('#initalPrompt').fadeOut();
+    		showFeature(data.cartodb_id)
+    		transition('#imageDate', data[config.column_names.date])
+    		transition('#rideQuality', data.ride_quality_score)
+    		
+    		$('#streetImg').fadeOut(function() {
+    			$('#loaderContainer').fadeIn();
+    			$('#streetImg')
+    			.attr('src', data[config.column_names.img_location])
+    			.load(function() {
+    				$('#loaderContainer').fadeOut(function() {
+    					$('#streetImg').fadeIn();
+    					$('#attributes').fadeIn();
+    				}
+    					);
+    			})
+    		});
+    		
+    		// $('#streetImg').attr('src', 'https://storage4.openstreetcam.org/files/photo/2017/5/30/proc/402532_f3ccd_592dbf20cbdd7.jpg')
+    		
+    		
+    	});
+    	globals.sublayers[0].on('featureOver', function(e, latlng, pos, data) {
+    		$("#map").css('cursor', 'pointer')
+    	});
+
+    	globals.sublayers[0].on('featureOut', function(e, latlng, pos, data) {
+    		$("#map").css('cursor','')
+    	});
+
+    });
+}
+
+
+// app build
+function main(){
+
+	// style setup
+	styleSetup();
+
+	// vizualization setup
+	mapSetup();
+
+}
